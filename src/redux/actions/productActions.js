513b6a6e844039c50a4185ad49e51de1a96d28ce
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-07-08 18:38:58 +0530

    created action types, actions, reducers and done with the redux store

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -2,7 +2,14 @@ import { ActionTypes } from "../contants/action-types";
 
 export const setProducts = (products) => {
     return {
-        type: ActionTypes.SET_PRODUCT,
-        payload: products
+        type: ActionTypes.SET_PRODUCTS,
+        payload: products,
+    }
+}
+
+export const selectedProduct = (product) => {
+    return {
+        type: ActionTypes.SELECTED_PRODUCT,
+        payload: product,
     }
 }
