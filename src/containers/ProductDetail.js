bb44cdff046fa7da924afbe9828b6e976e9416f8
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-07-09 09:31:03 +0530

    by doing api calls get all products and set in redux and build home page

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { selectedProduct } from "../redux/actions/productActions";
 
 const ProductDetail = () => {
+
+    const { productId } = useParams();
+    const dispatch = useDispatch();
+
+    const fetchProductDetail = () => {
+        const response = axios
+            .get(`https://fakestoreapi.com/products/${productId}`)
+            .catch((err) => {
+                console.log("Error ", err);
+            });
+
+            dispatch(selectedProduct(response.data));
+    };
 
     return (
     <div>
